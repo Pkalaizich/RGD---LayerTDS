f90368e238775cef4f87562c39963b4d261efe31
Pkalaizich <90625129+Pkalaizich@users.noreply.github.com>
2021-10-07 14:03:52 -0300

    First pull

---
+++ Layers TDS/RGD - Layers TDS/Assets/Scripts/Bullet.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public GameObject hitEffect;
+    public AudioClip hittingSound;
+    public AudioSource source;
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        GameObject effect = Instantiate(hitEffect, transform.position, Quaternion.identity);
+        source.PlayOneShot(hittingSound);
+        Destroy(effect, 2f);
+        Destroy(gameObject, 0.1f);
+    }
+}
