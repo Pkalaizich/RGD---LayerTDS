3a3ef781b3bfbe6d71f214dcd89d4c241e886287
Pkalaizich <90625129+Pkalaizich@users.noreply.github.com>
2021-10-30 16:09:30 -0300

    Ultimos cambios hablados

--- Layers TDS/RGD - Layers TDS/Assets/Scripts/HealthBar.cs
+++ Layers TDS/RGD - Layers TDS/Assets/Scripts/HealthBar.cs
@@ -7,9 +7,19 @@ public class HealthBar : MonoBehaviour
 {
     public Slider slider;
     public Life life;
+    public Image fill;
+    public Color original;
        
     public void SetHealth()
     {
         slider.value = life.life / life.maxLife;
+        StartCoroutine("changeColour");
+    }
+
+    IEnumerator changeColour()
+    {
+        fill.color = Color.red;
+        yield return new WaitForSeconds(0.5f);
+        fill.color = original;
     }
 }
